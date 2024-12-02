b95e8c7c6ee503d9e4736fdc674f55196ea13e9a
Admin <Admin@DESKTOP-UGP0C2G>
2024-12-02 13:49:52 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,10 @@
+﻿        Console.WriteLine("Введите значение a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        double z = Max(a, 2 * b) + Max(2 * a - b, b);
+        Console.WriteLine($"Значение z: {z}");
+    static double Max(double x, double y)
+    {
+        return (x > y) ? x : y;
+    }
